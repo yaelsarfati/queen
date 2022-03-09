6963444a7bcc93aa4c6248f718c47da991f34677
User <yaelsa2005@gmail.com>
2022-03-09 19:56:15 +0200

    i write the foncation

---
+++ yael.py
@@ -0,0 +1,8 @@
+one = input('if  you want live shoam write this')
+def live_or_not(one):
+    if one == 'yes':
+        print('you live now shoam')
+    else:
+        print(('you doesnt want live shoam'))
+
+live_or_not(one)
